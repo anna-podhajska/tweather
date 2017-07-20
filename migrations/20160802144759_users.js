52af165e3d6d3434291c6c9cedb488c2ec62b144
Anna P <podhajskaa@gmail.com>
2017-07-20 15:00:23 +1200

    added users, comments migration and seeds

--- migrations/20160802144759_users.js
+++ migrations/20160802144759_users.js
@@ -1,11 +1,11 @@
 exports.up = function (knex, Promise) {
-  return knex.schema.createTable('users', function (table) {
+  return knex.schema.createTable('weather', function (table) {
     table.increments('id').primary()
-    table.string('name')
-    table.string('email')
+    table.string('weather')
+    table.string('description')
   })
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.dropTable('users')
+  return knex.schema.dropTable('weather')
 }
